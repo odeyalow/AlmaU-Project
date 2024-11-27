064e81b58d9c54cb2230c30f3056b25a126754d9
Aldiyar <nesquikyey@gmail.com>
2024-11-27 14:33:37 +0500

    finished faq section

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -1,4 +1,18 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const question = document.querySelectorAll('.faq__question');
+
+    question.forEach(item => {
+        item.addEventListener('click', e => {
+            const currentQuestion = e.target.closest('.faq__question');
+            currentQuestion.children[0].children[1].classList.toggle('question__cross-active');
+            if (currentQuestion.children[1].style.maxHeight) {
+                currentQuestion.children[1].style.maxHeight = null;
+                // currentQuestion.children[1].style.overflow="hidden";
+            } else {
+                currentQuestion.children[1].style.maxHeight = currentQuestion.children[1].scrollHeight + "px";
+            }
+        }) 
+    });
 })
