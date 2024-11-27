7f2a94babb24b78e839ba386102184179481f88e
Aldiyar <nesquikyey@gmail.com>
2024-11-27 16:11:53 +0500

    added scroll to info section in hero

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -1,15 +1,23 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const question = document.querySelectorAll('.faq__question');
+    const scrollBtn = document.querySelector('.hero__scroll-btn'),
+        infoSection = document.querySelector('.info'),
+        question = document.querySelectorAll('.faq__question');
+
+    scrollBtn.addEventListener('click', () => {
+        infoSection.scrollIntoView({
+            block:'start',
+            behavior:'smooth'
+        });
+    })
 
     question.forEach(item => {
-        item.addEventListener('click', e => {
-            const currentQuestion = e.target.closest('.faq__question');
+         item.addEventListener('click', e => {
+           const currentQuestion = e.target.closest('.faq__question');
             currentQuestion.children[0].children[1].classList.toggle('question__cross-active');
             if (currentQuestion.children[1].style.maxHeight) {
                 currentQuestion.children[1].style.maxHeight = null;
-                // currentQuestion.children[1].style.overflow="hidden";
             } else {
                 currentQuestion.children[1].style.maxHeight = currentQuestion.children[1].scrollHeight + "px";
             }
